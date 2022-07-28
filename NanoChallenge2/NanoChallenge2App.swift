d557f3c59057afabd6a76a76b04daf81e236b506
Zevira Varies Martan <zeviravm@gmail.com>
2022-07-29 01:04:05 +0700

    Initial Commit

---
+++ NanoChallenge2/NanoChallenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge2App.swift
+//  NanoChallenge2
+//
+//  Created by Zevira varies martan on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
